d571eb57c6c2850020929e06818a5d0d0bd05080
tanvir <tanvirulislam149@gmail.com>
2025-08-05 14:47:13 +0600

    msg get fixed

--- message/views.py
+++ message/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from message.models import Message
 from message.serializers import MessageSerializer, CreateMessageSerializer
+from django.db.models import Q
 
 # Create your views here.
 class MessageViewSet(ModelViewSet):
@@ -14,5 +15,8 @@ class MessageViewSet(ModelViewSet):
         return MessageSerializer
 
     def get_queryset(self):   # get message url => /message/?receiver=1
-        receiver_id = self.request.query_params.get("receiver")
-        return Message.objects.filter(sender=self.request.user, receiver_id=receiver_id)
+        if self.request.user.is_staff:
+            return Message.objects.select_related("sender").select_related("receiver").all()
+        else:
+            receiver_id = self.request.query_params.get("receiver")
+            return Message.objects.select_related("sender").select_related("receiver").filter(Q(sender=self.request.user, receiver_id=receiver_id) | Q(sender=receiver_id, receiver_id=self.request.user))
