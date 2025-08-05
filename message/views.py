7fd702a7f2020ed6d7c25a02b6900c236603e598
tanvir <tanvirulislam149@gmail.com>
2025-08-05 23:26:48 +0600

    message get done

--- message/views.py
+++ message/views.py
@@ -20,11 +20,8 @@ class MessageViewSet(ModelViewSet):
         return MessageSerializer
 
     def get_queryset(self):   # get message url => /message/?receiver=1
-        if self.request.user.is_staff:
-            return Message.objects.select_related("sender").select_related("receiver").all()
-        else:
-            receiver_id = self.request.query_params.get("receiver")
-            return Message.objects.select_related("sender").select_related("receiver").filter(Q(sender=self.request.user, receiver_id=receiver_id) | Q(sender=receiver_id, receiver_id=self.request.user))
+        receiver_id = self.request.query_params.get("receiver")
+        return Message.objects.select_related("sender").select_related("receiver").filter(Q(sender=self.request.user, receiver_id=receiver_id) | Q(sender=receiver_id, receiver_id=self.request.user))
         
 
 @api_view(['GET'])
