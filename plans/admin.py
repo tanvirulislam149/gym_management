76bc7ebaceabed451dfa5fb77d01ffdbad89f05b
tanvir <tanvirulislam149@gmail.com>
2025-09-04 21:08:26 +0600

    review admin added

--- plans/admin.py
+++ plans/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from plans.models import Plans, Fitness_classes_category
+from plans.models import Plans, Fitness_classes_category, Scheduled_classes, Review
 
 # Register your models here.
 admin.site.register(Plans)
 admin.site.register(Fitness_classes_category)
+admin.site.register(Scheduled_classes)
+admin.site.register(Review)
